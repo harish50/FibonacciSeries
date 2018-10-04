aa97f21c4cb41d81840aea980440397594914e46
S Harish <sharish@akamai.com>
2018-10-04 22:34:17 +0530

    Returns 1 for 2 in FibonacciSeries
    Changed the if condition to check whether given number is 2

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class FibonacciSeries {
+    public int getNthNumber(int number) {
+        if (number == 1 || number ==2){
+            return 1;
+        }
+        return -1;
+    }
 }
--- src/FibonacciSeriesTest.java
+++ src/FibonacciSeriesTest.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class FibonacciSeriesTest {
+    @Test
+    public void fibonacciSeriesNthNumberForOne(){
+        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+        Assert.assertEquals(1,fibonacciSeries.getNthNumber(1));
+    }
+    @Test
+    public void fibonacciSeriesNthNumberForTwo(){
+        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+        Assert.assertEquals(1,fibonacciSeries.getNthNumber(2));
+    }
 }
